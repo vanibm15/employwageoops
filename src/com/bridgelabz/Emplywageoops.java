95e163d38e1c779439f98ccd524b3504d2023570
vanibm15 <vanibm15@gmail.com>
2022-04-28 22:43:25 +0530

    added:parttime employwage

--- src/com/bridgelabz/Emplywageoops.java
+++ src/com/bridgelabz/Emplywageoops.java
@@ -15,9 +15,10 @@ public class Emplywageoops {
         else
             System.out.println("Employee is absent");
     }
+
     //UC2
     //creating another method
-    void dailyEmploywage(){
+    void dailyEmploywage() {
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == IS_FULL_TIME)
             empHrs = 8;
@@ -27,11 +28,30 @@ public class Emplywageoops {
         System.out.println("Emp wage: " + empwage);
     }
 
+    //UC3-Method three
+    static void partTimeEmployWage() {
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empwage = 0;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PART_TIME)
+            empHrs = 4;
+        else if (empcheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empwage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empwage);
+
+    }
+
     public static void main(String[] args) {
         method1();//calling 1st method
-        Emplywageoops obj1=new Emplywageoops();//calling 2nd method
+        Emplywageoops obj1 = new Emplywageoops();//calling 2nd method
         obj1.dailyEmploywage();
-
+        partTimeEmployWage();//calling method 3;
     }
 }
 
