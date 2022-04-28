d87c9bb13e95024b5e870215f209d2f7f3d16170
vanibm15 <vanibm15@gmail.com>
2022-04-28 22:36:35 +0530

    added:daily employewage

--- src/com/bridgelabz/Emplywageoops.java
+++ src/com/bridgelabz/Emplywageoops.java
@@ -1,6 +1,12 @@
 package com.bridgelabz;
 
 public class Emplywageoops {
+    int EMP_RATE_PER_HOUR = 20;
+    int empHrs = 0;
+    int empwage = 0;
+    int IS_FULL_TIME = 1;
+
+    //UC1
     static void method1() {
         int IS_FULL_TIME = 1;
         double empcheck = Math.floor(Math.random() * 10) % 2;
@@ -9,11 +15,27 @@ public class Emplywageoops {
         else
             System.out.println("Employee is absent");
     }
+    //UC2
+    //creating another method
+    void dailyEmploywage(){
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empwage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empwage);
+    }
 
     public static void main(String[] args) {
-        method1();
+        method1();//calling 1st method
+        Emplywageoops obj1=new Emplywageoops();//calling 2nd method
+        obj1.dailyEmploywage();
+
     }
 }
+
+
 
 
 
